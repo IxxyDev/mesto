cc95bc9257bce39142e00dd77f8e9659da49cadf
IxxyDev <denisenkoforwork@gmail.com>
2020-08-09 02:01:11 +0300

    refactor: delete form from PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,17 +6,10 @@ function PopupWithForm({ name, isOpened, title, children, onClose, onSubmit }) {
         <div className="popup__content">
           <button className="popup__close-button popup__close-button_type_edit-profile" onClick={onClose} type="button"></button>
           <h3 className="popup__title">{title}</h3>
-          <form
-            method="POST"
-            onSubmit={onSubmit}
-            className="popup__form popup__form_type_edit-profile"
-            name={name}
-            noValidate>
             {children}
             <button
               className="popup__button popup__button_type_save"
               type="submit">Сохранить</button>
-          </form>
         </div>
       </section>
   )
